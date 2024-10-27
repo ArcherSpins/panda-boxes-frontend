de2a3c98e688fef6f68d409a3c0b5b2fb2204da6
misapanda <misapanda@proton.me>
2024-10-27 11:16:37 +0900

    Added clsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="p-5 bg-slate-400">
       <Button />
-      <Button color="white" label="Ivan" />
-      <Button color="gray" label="JS" />
+      <Button className="bg-white text-black" label="Ivan" />
+      <Button className="bg-gray-300 text-black" label="JS" />
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,16 @@
+import clsx from "clsx";
+
 interface ButtonProps {
-  color?: "black" | "white" | "gray";
+  className?: string;
   label?: string;
 }
 
-export const Button = ({ color = "black", label = "Button" }: ButtonProps) => {
-  const colors = {
-    black: "bg-black hover:bg-gray-300 text-white",
-    white: "bg-white text-black",
-    gray: "bg-gray-300 text-black",
-  };
-
+export const Button = ({
+  className = "bg-black hover:bg-gray-300 text-white",
+  label = "Button",
+}: ButtonProps) => {
   return (
-    <button className={`rounded-xl px-5 py-3 font-bold ${colors[color]}`}>
+    <button className={clsx("rounded-xl px-5 py-3 font-bold", className)}>
       {label}
     </button>
   );
